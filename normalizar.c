95c115d8b666c7446ed561a64070c80f2588bf21
Rafareib4 <149714197+Rafareib4@users.noreply.github.com>
2024-11-12 18:07:03 +0100

    Add files via upload

--- normalizar.c
+++ normalizar.c
@@ -1,44 +1,58 @@
 #include "normalizar.h"
 
 //Acción para encontrar los valores mínimos y máximos para la glucosa, bmi y edad
-void calcularMaxYMin(Patient pacientes[], MaxYMin* maximos_minimos, int num_pacientes){
+void calcularMaxYMin(Datos caballos[], MaxYMin* maximos_minimos, int num_caballos){
 
-    maximos_minimos->min_glucose = pacientes[0].glucose, maximos_minimos->max_glucose = pacientes[0].glucose;
-    maximos_minimos->min_bmi = pacientes[0].bmi, maximos_minimos->max_bmi = pacientes[0].bmi;
-    maximos_minimos->min_age = pacientes[0].age, maximos_minimos->max_age = pacientes[0].age;
+    maximos_minimos->minTemperaturaRectal = caballos[0].temperaturaRectal, maximos_minimos->maxTemperaturaRectal = caballos[0].temperaturaRectal;
+    maximos_minimos->minPulso = caballos[0].pulso, maximos_minimos->maxPulso = caballos[0].pulso;
+    maximos_minimos->minFrecuenciaResp = caballos[0].frecuenciaRespiracion, maximos_minimos->maxFrecuenciaResp = caballos[0].frecuenciaRespiracion;
+    maximos_minimos->minVolumenCelulas = caballos[0].volumenCelulas, maximos_minimos->maxVolumenCelulas = caballos[0].volumenCelulas;
+    maximos_minimos->minProteina = caballos[0].proteina, maximos_minimos->maxProteina = caballos[0].proteina;
 
-    for (int i = 0; i < num_pacientes; i++){
-        if (pacientes[i].glucose < maximos_minimos->min_glucose){
-            maximos_minimos->min_glucose = pacientes[i].glucose;
+    for (int i = 0; i < num_caballos; i++){
+        if (caballos[i].temperaturaRectal < maximos_minimos->minTemperaturaRectal){
+            maximos_minimos->minTemperaturaRectal = caballos[i].temperaturaRectal;
         }
-        if (pacientes[i].glucose > maximos_minimos->max_glucose){
-            maximos_minimos->max_glucose = pacientes[i].glucose;
+        if (caballos[i].temperaturaRectal > maximos_minimos->maxTemperaturaRectal){
+            maximos_minimos->maxTemperaturaRectal = caballos[i].temperaturaRectal;`
         }
-
-        if (pacientes[i].bmi < maximos_minimos->min_bmi){
-            maximos_minimos->min_bmi = pacientes[i].bmi;
+        if (caballos[i].pulso < maximos_minimos->minPulso){
+            maximos_minimos->minPulso = caballos[i].pulso;
         }
-        if (pacientes[i].bmi > maximos_minimos->max_bmi){
-            maximos_minimos->max_bmi = pacientes[i].bmi;
+        if (caballos[i].pulso > maximos_minimos->maxPulso){
+            maximos_minimos->maxPulso = caballos[i].pulso;
         }
-        if (pacientes[i].age < maximos_minimos->min_age){
-            maximos_minimos->min_age = pacientes[i].age;
+        if (caballos[i].frecuenciaRespiracion < maximos_minimos->minFrecuenciaResp){
+            maximos_minimos->minFrecuenciaResp = caballos[i].frecuenciaRespiracion;
         }
-        if (pacientes[i].age > maximos_minimos->max_age){
-            maximos_minimos->max_age = pacientes[i].age;
+        if (caballos[i].frecuenciaRespiracion > maximos_minimos->maxFrecuenciaResp){
+            maximos_minimos->maxFrecuenciaResp = caballos[i].frecuenciaRespiracion;
+        }
+        if (caballos[i].volumenCelulas < maximos_minimos->minVolumenCelulas){
+            maximos_minimos->minVolumenCelulas = caballos[i].volumenCelulas;
+        }
+        if (caballos[i].volumenCelulas > maximos_minimos->maxVolumenCelulas){
+            maximos_minimos->maxVolumenCelulas = caballos[i].volumenCelulas;
+        }
+        if (caballos[i].proteina < maximos_minimos->minProteina){
+            maximos_minimos->minProteina = caballos[i].proteina;
+        }
+        if (caballos[i].proteina > maximos_minimos->maxProteina){
+            maximos_minimos->maxProteina = caballos[i].proteina;
         }
     }
 }
 
 //Acción para normalizar los datos
-void normalizeData(struct Patient pacientes[], int num_pacientes) {
+void normalizeData(Datos caballos[], int num_caballos) {
 
     MaxYMin maximos_minimos;
-    calcularMaxYMin(pacientes, &maximos_minimos, num_pacientes);
-    for (int i = 0; i < num_pacientes; i++) {
-        pacientes[i].glucose = (pacientes[i].glucose - maximos_minimos.min_glucose) / (maximos_minimos.max_glucose - maximos_minimos.min_glucose);
-        pacientes[i].bmi = (pacientes[i].bmi - maximos_minimos.min_bmi) / (maximos_minimos.max_bmi - maximos_minimos.min_bmi);
-        pacientes[i].age = (pacientes[i].age - maximos_minimos.min_age) / (maximos_minimos.max_age - maximos_minimos.min_age);
-        pacientes[i].smoking_status = (pacientes[i].smoking_status)/2;
+    calcularMaxYMin(caballos, &maximos_minimos, num_caballos);
+    for (int i = 0; i < num_caballos; i++) {
+        caballos[i].temperaturaRectal = (caballos[i].temperaturaRectal - maximos_minimos.minTemperaturaRectal) / (maximos_minimos.maxTemperaturaRectal - maximos_minimos.minTemperaturaRectal);
+        caballos[i].pulso = (caballos[i].pulso - maximos_minimos.minPulso) / (maximos_minimos.maxPulso - maximos_minimos.minPulso);
+        caballos[i].frecuenciaRespiracion = (caballos[i].frecuenciaRespiracion - maximos_minimos.minFrecuenciaResp) / (maximos_minimos.maxFrecuenciaResp - maximos_minimos.minFrecuenciaResp);
+        caballos[i].volumenCelulas = (caballos[i].volumenCelulas - maximos_minimos.minVolumenCelulas) / (maximos_minimos.maxVolmmenCelulas-maximos_minimos.minVolumenCelulas);
+        caballos[i].proteina = (caballos[i].proteina - maximos_minimos.minProteina) / (maximos_minimos.maxProteina-maximos_minimos.minProteina);
     }
 }
