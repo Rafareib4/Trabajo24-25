8ac3c32cfd6ca38c2690742bf2339da5349b443e
Rafareib4 <149714197+Rafareib4@users.noreply.github.com>
2024-11-19 18:19:08 +0100

    Add files via upload

--- knn_wilson.c
+++ knn_wilson.c
@@ -12,42 +12,29 @@ bool convert_string_to_bool(char* textoFragmentado){
     return strcmp(textoFragmentado, "1") == 0 || strcmp(textoFragmentado, "1\n") == 0;
 }
 
-void imprimirDatos(Patient pacientes[], int num_pacientes){
-    for (int i = 0; i < num_pacientes; i++){
+void imprimirDatos(Patient caballos[], int num_caballos){
+    for (int i = 0; i < num_caballos; i++){
         printf("Datos del paciente %d: ", i + 1);
-        printf("    Género: %s ", pacientes[i].gender);
-        printf("    Edad: %.0f", pacientes[i].age);
-        printf("    Hipertensión: %d ", (int)pacientes[i].hypertension);
-        printf("    Enfermedad cardíaca: %d ", (int)pacientes[i].heart_disease);
-        printf("    Casado alguna vez: %d ", (int)pacientes[i].ever_married);
-        printf("    Tipo de trabajo: %s ", pacientes[i].work_type);
-        printf("    Residencia: %s ", pacientes[i].residence);
-        printf("    Glucosa: %.2f ", pacientes[i].glucose);
-        printf("         BMI: %.2f ", pacientes[i].bmi);
-        printf("    Estado de fumador: %.0f", pacientes[i].smoking_status);
-        printf("    Derrame cerebral: %d ", (int)pacientes[i].stroke);
+        printf("    Edad: %s ", caballos[i].edad);
+        printf("    Temperatura Rectal: %.2f ", caballos[i].temperaturaRectal);
+        printf("    Pulso: %.2f ", caballos[i].pulso);
+        printf("    Frecuencia Respiracion: %.2f ", caballos[i].frecuenciaRespiracion);
+        printf("    Temperatura Extremidades: %s ", caballos[i].temperaturaExtremidades);
+        printf("    Dolor: %s ", caballos[i].dolor);
+        printf("    Distension Abdominal: %s ", caballos[i].distensionAbdominal);
+        printf("    Tubo Nasogastrico: %s ", caballos[i].tuboNasogastrico);
+        printf("    Heces: %s ", caballos[i].heces);
+        printf("    Volumen Celular: %.2f ", caballos[i].volumenCelulas);
+        printf("    Proteina: %.2f ", caballos[i].proteina);
+        printf("    Operado: %d ", (int)caballos[i].cirugia);
+        printf("    Lesion: %d ", (int)caballos[i].lesionCirugia);
+        printf("    Resultado: %d", (int)caballos[i].resultado);
         printf("\n");
     }
 }
 
-void imprimirDatosNormalizados(Patient pacientes[], int num_pacientes){
-    for (int i = 0; i < num_pacientes; i++) {
-        printf("Datos normalizados del paciente %d: ", i + 1);
-        printf("    genero: %s",pacientes[i].gender);
-        printf("    edad: %.2f", pacientes[i].age);
-        printf("    hipertension: %d",(int)pacientes[i].hypertension);
-        printf("    enfermedad cardíaca: %d",(int)pacientes[i].heart_disease);
-        printf("    residencia: %s", pacientes[i].residence);
-        printf("    trabajo: %s", pacientes[i].work_type);
-        printf("    Glucosa: %.2f", pacientes[i].glucose);
-        printf("    BMI: %.2f", pacientes[i].bmi);
-        printf("    fumador: %.2f", pacientes[i].smoking_status);
-        printf("    Derrame cerebral: %d\n",(int)pacientes[i].stroke);
-    }
-}
-
 int main(){
-    FILE *archivo = fopen("dataset.csv", "r");
+    FILE *archivo = fopen("datasetDefinitivo.csv", "r");
     char lineaTexto[100];
     char** textoFragmentado;
     if (archivo == NULL) {
@@ -55,54 +42,53 @@ int main(){
         exit(-1); // Salida con error
     }
 
-    struct Patient pacientes[MAX_PATIENTS]; //Constante MAX_PATIENTS declarada en normalizar.h
 
-    int num_pacientes = 0;
+    int num_caballos = 0;
     char res;
 
-    // Lee los datos del archivo y los almacena en un array de pacientes
-    while(fgets(lineaTexto, 100, archivo) != NULL && num_pacientes < MAX_PATIENTS){
+    // Lee los datos del archivo y los almacena en un array de caballos
+    while(fgets(lineaTexto, 100, archivo) != NULL && num_caballos < MAX_CABALLOS){
         textoFragmentado = fragmenta(lineaTexto);
-        pacientes[num_pacientes].num = num_pacientes;
-        strcpy(pacientes[num_pacientes].gender, textoFragmentado[0]);
-        pacientes[num_pacientes].age = atoi(textoFragmentado[1]);
-        pacientes[num_pacientes].hypertension = convert_string_to_bool(textoFragmentado[2]);
-        pacientes[num_pacientes].heart_disease = convert_string_to_bool(textoFragmentado[3]);
-        pacientes[num_pacientes].ever_married = convert_string_to_bool(textoFragmentado[4]);
-        strcpy(pacientes[num_pacientes].work_type, textoFragmentado[5]);
-        strcpy(pacientes[num_pacientes].residence, textoFragmentado[6]);
-        pacientes[num_pacientes].glucose = atof(textoFragmentado[7]);
-        pacientes[num_pacientes].bmi = atof(textoFragmentado[8]);
-        pacientes[num_pacientes].smoking_status = atoi(textoFragmentado[9]);
-        pacientes[num_pacientes].stroke = convert_string_to_bool(textoFragmentado[10]);
-        num_pacientes++;
+        caballos[num_caballos].num = num_caballos;
+        caballos[num_caballos].cirugia = convert_string_to_bool(textoFragmentado[0]);
+        strcpy(caballos[num_caballos].edad, textoFragmentado[1]);
+        caballos[num_caballos].temperaturaRectal = atof(textoFragmentado[2]);
+        caballos[num_caballos].pulso = atof(textoFragmentado[3]);
+        caballos[num_caballos].frecuenciaRespiracion = atof(textoFragmentado[4]);
+        strcpy(caballos[num_caballos].temperaturaExtremidades, textoFragmentado[5]);
+        strcpy(caballos[num_caballos].dolor, textoFragmentado[6]);
+        strcpy(caballos[num_caballos].distensionAbdominal, textoFragmentado[7]);
+        strcpy(caballos[num_caballos].tuboNasogastrico, textoFragmentado[8]);
+        strcpy(caballos[num_caballos].heces, textoFragmentado[9]);
+        caballos[num_caballos].volumenCelulas = atof(textoFragmentado[10]);
+        caballos[num_caballos].proteina = atof(textoFragmentado[11]);
+        caballos[num_caballos].resultado = convert_string_to_bool(textoFragmentado[12]);
+        caballos[num_caballos].lesionCirugia = convert_string_to_bool(textoFragmentado[13]);
+        num_caballos++;
     }
 
-    // Imprime los datos de los pacientes en la consola
-    //imprimirDatos(pacientes, num_pacientes);
+    // Imprime los datos de los caballos en la consola
+    imprimirDatos(caballos, num_caballos);
 
-    normalizeData(pacientes, num_pacientes);
-
-    // Imprimir los datos normalizados
-    //imprimirDatosNormalizados(pacientes, num_pacientes);
+    normalizeData(caballos, num_caballos);
 
     //Calcular distancias
-    float dist_age, dist_bmi, dist_glucose, dist_smoking_status, dist_total, dist_gender, dist_hypertension, dist_heart_disease, dist_ever_married, dist_work_type, dist_residence;
+    float dist_edad, dist_cirugia, dist_temperaturaRectal, dist_pulso, dist_total, dist_frecuenciaRespiracion, dist_temperaturaExtremidades, dist_dolor, dist_distensionAbdominal, dist_tuboNasogastrico, dist_heces, dist_volumenCelulas, dist_proteina, dist_resultado, dist_lesionCirugia;
     tipoLista lista;
     distanciaYCaso elementoLista;
-    int knn, k;
+    int numKnn, k;
     int contador1, contador0, contadorAciertos, contadorFallos, contadorWilson;
     float probabilidad;
     bool prediccion, check;
     char resW;
-    knn = num_pacientes;
+    knn = num_caballos;
     contadorWilson = 0;
     do{
         contadorAciertos = 0;
         contadorFallos = 0;
-        printf("Qué número de K quieres usar (1 <= K < %d): ", knn);
+        printf("Qué número de K quieres usar (1 <= K < %d): ", numKnn);
         scanf("%d", &k);
-        while(k >= knn || k < 1){
+        while(k >= numKnn || k < 1){
             printf("Error, la K no es válida introdúcela de nuevo (1 <= K < %d): ", knn);
             scanf("%d", &k);
         }
@@ -110,52 +96,67 @@ int main(){
         scanf(" %c", &resW);
         int indice1, indice2;
         nuevaLista(&lista);
-        for (indice1 = 0; indice1 < num_pacientes; indice1++){
+        for (indice1 = 0; indice1 < num_caballos; indice1++){
             contador1 = 0;
             contador0 = 0;
-            while(pacientes[indice1].num == SALTAR_FALLOS){
+            while(caballos[indice1].num == SALTAR_FALLOS){
                 indice1++;
             }
-            for (indice2 = 0; indice2 < num_pacientes; indice2++){
-                while(indice1 == indice2 || pacientes[indice2].num == SALTAR_FALLOS){
+            for (indice2 = 0; indice2 < num_caballos; indice2++){
+                while(indice1 == indice2 || caballos[indice2].num == SALTAR_FALLOS){
                     indice2++;
                 }
-                dist_gender = 1;
-                dist_hypertension =1;
-                dist_heart_disease = 1;
-                dist_ever_married = 1;
-                dist_work_type = 1;
-                dist_residence = 1;
-                dist_age = fabs(pacientes[indice1].age - pacientes[indice2].age);
-                dist_glucose = fabs(pacientes[indice1].glucose - pacientes[indice2].glucose);
-                dist_bmi = fabs(pacientes[indice1].bmi - pacientes[indice2].bmi);
-                dist_smoking_status = fabs(pacientes[indice1].smoking_status - pacientes[indice2].smoking_status);
-                if(pacientes[indice1].hypertension == pacientes[indice2].hypertension){
-                    dist_hypertension = 0;
+                dist_cirugia = 1;
+                dist_edad =1;
+                dist_temperaturaExtremidades = 1;
+                dist_dolor = 1;
+                dist_distensionAbdominal = 1;
+                dist_tuboNasogastrico = 1;
+                dist_heces = 1;
+                dist_resultado = 1;
+                dist_lesionCirugia = 1;
+
+                dist_temperaturaRectal = fabs(caballos[indice1].temperaturaRectal - caballos[indice2].temperaturaRectal);
+                dist_pulso = fabs(caballos[indice1].pulso - caballos[indice2].pulso);
+                dist_frecuenciaRespiracion = fabs(caballos[indice1].frecuenciaRespiracion - caballos[indice2].frecuenciaRespiracion);
+                dist_volumenCelulas = fabs(caballos[indice1].volumenCelulas - caballos[indice2].volumenCelulas);
+                dist_proteina = fabs(caballos[indice1].proteina-caballos[indice2].proteina);
+
+                if(caballos[indice1].cirugia == caballos[indice2].cirugia){
+                    dist_cirugia = 0;
                 }
-                if(pacientes[indice1].heart_disease == pacientes[indice2].heart_disease){
-                    dist_heart_disease = 0;
+                if(caballos[indice1].resultado == caballos[indice2].resultado){
+                    dist_resultado = 0;
                 }
-                if(pacientes[indice1].ever_married == pacientes[indice2].ever_married){
-                    dist_ever_married = 0;
+                if(caballos[indice1].lesionCirugia == caballos[indice2].lesionCirugia){
+                    dist_lesionCirugia = 0;
                 }
-                if(strcmp(pacientes[indice1].gender, pacientes[indice2].gender)== 0){
-                    dist_gender = 0;
+                if(strcmp(caballos[indice1].dolor, caballos[indice2].dolor)== 0){
+                    dist_dolor = 0;
                 }
-                if(strcmp(pacientes[indice1].work_type, pacientes[indice2].work_type)== 0){
-                    dist_work_type = 0;
+                if(strcmp(caballos[indice1].distensionAbdominal, caballos[indice2].distensionAbdominal)== 0){
+                    dist_distensionAbdominal = 0;
                 }
-                if(strcmp(pacientes[indice1].residence, pacientes[indice2].residence)== 0){
-                    dist_residence = 0;
+                if(strcmp(caballos[indice1].edad, caballos[indice2].edad)== 0){
+                    dist_edad = 0;
                 }
-                dist_total = sqrt(dist_age * dist_age + dist_bmi * dist_bmi + dist_glucose * dist_glucose + dist_smoking_status * dist_smoking_status + dist_ever_married + dist_gender + dist_heart_disease + dist_hypertension + dist_residence + dist_work_type);
+                if(strcmp(caballos[indice1].temperaturaExtremidades, caballos[indice2].temperaturaExtremidades)== 0){
+                    dist_temperaturaExtremidades = 0;
+                }
+                if(strcmp(caballos[indice1].tuboNasogastrico, caballos[indice2].tuboNasogastrico)== 0){
+                    dist_tuboNasogastrico = 0;
+                }
+                if(strcmp(caballos[indice1].heces, caballos[indice2].heces)== 0){
+                    dist_heces = 0;
+                }
+                dist_total = sqrt(dist_temperaturaRectal * dist_temperaturaRectal + dist_pulso * dist_pulso + dist_frecuenciaRespiracion * dist_frecuenciaRespiracion + dist_volumenCelulas * dist_volumenCelulas + dist_proteina*dist_proteina + dist_cirugia + dist_resultado + dist_lesionCirugia + dist_dolor + dist_distensionAbdominal + dist_edad + dist_temperaturaExtremidades + dist_tuboNasogastrico + dist_heces);
                 elementoLista.distancia = dist_total;
-                elementoLista.stroke = pacientes[indice2].stroke;
+                elementoLista.stroke = caballos[indice2].stroke;
                 insertar(&lista, elementoLista);
             }
-            check = lista.ini->elemento.stroke;
+            check = lista.ini->elemento.resultado;
             for (indice2 = 0; indice2 < k; indice2++){
-                if(lista.ini->elemento.stroke == 1){
+                if(lista.ini->elemento.resultado == 1){
                     contador1++;
                 }
                 else{
@@ -172,33 +173,33 @@ int main(){
             else if (contador0 == contador1){
                 prediccion = check;
             }
-            if (prediccion == pacientes[indice1].stroke){
+            if (prediccion == caballos[indice1].resultado){
                 contadorAciertos++;
             }
-            else if(prediccion != pacientes[indice1].stroke && (resW == 's' || resW == 'S')){
+            else if(prediccion != caballos[indice1].resultado && (resW == 's' || resW == 'S')){
                 contadorFallos++;
-                pacientes[indice1].num = NUM_FALLOS;
+                caballos[indice1].num = NUM_FALLOS;
 
             }
             if(!esNulaLista(lista)){
                 eliminarTodosLosElementos(&lista);
             }
         }
-        probabilidad = ((float)contadorAciertos / (float)knn) * 100;
+        probabilidad = ((float)contadorAciertos / (float)numKnn) * 100;
         if(resW == 's' || resW == 'S'){
-            for(int i = 0; i < num_pacientes; i++){
-                if(pacientes[i].num == NUM_FALLOS){
-                    pacientes[i].num = SALTAR_FALLOS;
+            for(int i = 0; i < num_caballos; i++){
+                if(caballos[i].num == NUM_FALLOS){
+                    caballos[i].num = SALTAR_FALLOS;
                 }
             }
             contadorWilson++;
-            printf("La probabilidad de acierto para %d casos con K = %d, con %d wilson es: %.5f%%\n", knn, k, contadorWilson - 1, probabilidad);
+            printf("La probabilidad de acierto para %d casos con K = %d, con %d wilson es: %.5f%%\n", numKnn, k, contadorWilson - 1, probabilidad);
         }
         else{
-            printf("La probabilidad de acierto para %d casos con K = %d, con %d wilson es: %.5f%%\n", knn, k, contadorWilson, probabilidad);
+            printf("La probabilidad de acierto para %d casos con K = %d, con %d wilson es: %.5f%%\n", numKnn, k, contadorWilson, probabilidad);
         }
-        printf("numero de aciertos: %d   numero de datos: %d\n", contadorAciertos, knn);
-        knn = knn - contadorFallos;
+        printf("numero de aciertos: %d   numero de datos: %d\n", contadorAciertos, numKnn);
+        numKnn = numKnn - contadorFallos;
         printf("Deseas ejecutar otra vez knn s/n: ");
         scanf(" %c", &res);
     } while (res == 's' || res == 'S');
